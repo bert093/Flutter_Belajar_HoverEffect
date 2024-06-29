5b605daf4ab22411866d46af0f6bb34025086ebf
bert093 <sas992203@gmail.com>
2024-06-29 19:33:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_belajar_hover
+ https://youtu.be/2zgbXCOcCOI?si=PGbPuPTGbVsFVSdx
---
+++ android/app/src/main/kotlin/com/example/flutter_belajar_hover/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_belajar_hover
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
